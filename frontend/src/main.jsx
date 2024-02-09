8f2b9ba4da43c827dca070f57388530840ecf52b
Allisson100 <allisson.boiane@outlook.com>
2024-02-09 20:01:05 -0300

    Front: Cria gerenciamento de rotas com react-router-dom

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import GlobalStyles from "./styles/GlobalStyles.jsx";
 import { ThemeProvider } from "styled-components";
 import theme from "./styles/Theme.js";
+import { Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<ThemeProvider theme={theme.shadesBlue}>
 			<GlobalStyles />
-			<App />
+			<Router />
 		</ThemeProvider>
 	</React.StrictMode>,
 );
