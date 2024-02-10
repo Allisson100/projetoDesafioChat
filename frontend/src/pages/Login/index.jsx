da6cfe7814a625bbe973c0a5dd05fce20c5ac539
Allisson100 <allisson.boiane@outlook.com>
2024-02-10 12:49:28 -0300

    Front: Cria a estrutura da pagina de LogIn

---
+++ frontend/src/pages/Login/index.jsx
@@ -0,0 +1,22 @@
+const Login = () => {
+	return (
+		<main>
+			<section>
+				<div>Logo</div>
+				<h3>Texto</h3>
+				<form>
+					<input type="text" />
+					<input type="passsword" />
+					<button>Log In</button>
+				</form>
+			</section>
+			<section>
+				<h2>Titulo</h2>
+				<h4>Texto</h4>
+				<button>Sign Up</button>
+			</section>
+		</main>
+	);
+};
+
+export default Login;
